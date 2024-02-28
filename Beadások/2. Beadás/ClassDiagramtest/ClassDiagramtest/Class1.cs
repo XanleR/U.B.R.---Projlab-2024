2f97d1cb6b2ca348f746b0b16d32fd210535339d
XanleR <bogardanischool@gmail.com>
2024-02-28 10:47:16 +0100

    1. beadás

---
+++ Beadások/2. Beadás/ClassDiagramtest/ClassDiagramtest/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassDiagramtest
+{
+    public class Class1 : Program, Interface1
+    {
+        public Interface1 Interface1
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
---
+++ Beadások/2. Beadás/ClassDiagramtest/ClassDiagramtest/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ClassDiagramtest
+{
+    internal abstract class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

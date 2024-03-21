55b1aab91b7f3009390f06f3ee66b47f8b7d8538
Varjú Zalán Péter <VarjZalan@gmail.com>
2024-03-21 15:09:28 +0100

    Student and Character class finish(there are things todo in comments)

--- The_Migthy_Game_Of_UBR/src/Characters/Character.java
+++ The_Migthy_Game_Of_UBR/src/Characters/Character.java
@@ -14,45 +14,45 @@ public abstract class Character {
     private int stunnedRounds;
 
     //Getter: Visszaadja a szobat, amelyben a karakter all
-    public Room GetRoom(){
+    public Room getRoom(){
         return this.currentRoom;
     }
 
     //Setter: A parameterkent kapott int-re beallitja a stunnedRounds
-    public void Stun(int stun){
+    public void stun(int stun){
         this.stunnedRounds = stun;
     }
 
     //input: Room from, Room to
     //method: A karaktert athelyezi az egyik bemenetkent adott szobabol a masikba
     //return: void
-    public abstract void Move(Room from, Room to);
+    public abstract void move(Room from, Room to);
 
     //input: Item newI
     //method: A parameterkent megadott Item-et Student eseten berkaja a Student List<item>-ebe, Instructor eseten kitorli
     //        az Item-et a jatekbol
     //return: void
-    public abstract void PickUpItem(Item newI);
+    public abstract void pickUpItem(Item newI);
 
     //input: -
     //method: vegrehajtja a felhasznalo altal kivalasztott action-t
     //return: void
-    public abstract void Action();
+    public abstract void action();
 
     //input: -
     //method: Vegrehajtja azt az esemenyt, amikor a peldany egy Student-el kerul egy mezore
     //return: void
-    public abstract void MeetStudent();
+    public abstract void meetStudent();
 
     //input: -
     //method: Vegrehajtja azt az esemenyt, amikor a pedany egy Instructor-el kerul egy mezore
     //return: void
-    public abstract void MeetInstructor();
+    public abstract void meetInstructor();
 
     //input: -
     //method: Elindítja a játékos körét, és meghívja a paraméterként kapott számmal az ‘action’ függvényt
     //return: void
-    public abstract void StartRound(int in);
+    public abstract void startRound(int in);
 }
 
 
--- The_Migthy_Game_Of_UBR/src/Characters/Student.java
+++ The_Migthy_Game_Of_UBR/src/Characters/Student.java
@@ -2,41 +2,113 @@ package Characters;
 
 public class Student extends Character {
 
+    //Megadja, hogy a peldany vedelem alatt all-e
+    private boolean isProtected;
+
+    //Megadja, hogy hany kor van meg hatra az FFP2 targy felhasznalasabol
+    private int maskedRounds;
+
+    //Eltarolja a Student altal felvett targyakat;
+    private List<Item> inventory;
+
+    //Eltarolja a Student-nel levo Transistorokat
+    private List<Transistor> transistorList;
+
+    //TODO comment here
+    private Transistor transistor;
+
+    //Megadja, hogy a Student, hany Item-et vehet-fel
+    private int inventorySize;
 
     //input: Room from, Room to
     //method: A karaktert athelyezi az egyik bemenetkent adott szobabol a masikba
     //return: void
     @Override
-    public void Move(Room from, Room to) {}
+    public void move(Room from, Room to) {}
 
     //input: Item newI
     //method: A parameterkent megadott Item-et Student eseten berkaja a Student List<item>-ebe, Instructor eseten kitorli
     //        az Item-et a jatekbol
     //return: void
     @Override
-    public void PickUpItem(Item newI){}
+    public void pickUpItem(Item newI){}
 
     //input: -
     //method: vegrehajtja a felhasznalo altal kivalasztott action-t
     //return: void
     @Override
-    public void Action() {}
+    public void action() {}
 
     //input: -
     //method: Vegrehajtja azt az esemenyt, amikor a peldany egy Student-el kerul egy mezore
     //return: void
     @Override
-    public void MeetStudent() {}
+    public void meetStudent() {}
 
     //input: -
     //method: Vegrehajtja azt az esemenyt, amikor a pedany egy Instructor-el kerul egy mezore
     //return: void
     @Override
-    public void MeetInstructor() {}
+    public void meetInstructor() {}
 
     //input: -
     //method: Elindítja a játékos körét, és meghívja a paraméterként kapott számmal az ‘action’ függvényt
     //return: void
     @Override
-    public void StartRound(int in) {}
+    public void startRound(int in) {}
+
+    //input: Item dropped
+    //method: Kitorli a targyat a Student inventory-jabol es hozzaadja a szoba targyakat tarolo attributumahoz
+    //return: void
+    public void dropItem(Item dropped){}
+
+    //input: Item used
+    //method: Egy tárgy alkalmazását megvalósító metódus
+    //return: void
+    public void useItem(Item used){}
+
+    //input: -
+    //method: Egy véletlenszerű egész számot generál az 1-től 6-ig terjedő zárt intervallumon.
+    //return: int
+    public int rollDice(){ return 0;}
+
+    //input: -
+    //method: A tranzisztorral való utazást megvalósító függvén
+    //return: void
+    public void transistorJump(){}
+
+    //intput: Transistor newT
+    //method: A tranzisztorok listájába beleteszi a paraméterként kapott tranzisztort
+    //return: void
+    public void addTransistor(Transistor newT){}
+
+    //input: int index
+    //method:  Visszaadja a tranzisztorok listájából az indexedik tranzisztort
+    //return: Transistor
+    public Transistor getTransistor(int index){
+        return null;
+    }
+
+    //input: int plus
+    //method: TODO here, method
+    //return: void
+    public void addRounds(int plus){}
+
+    //input: int plusMask
+    //method: Hozzaadja a bemenetkent kapott int-et a maskedRounds attributumhoz
+    //return: void
+    public void addMaskedRounds(int plusMask){
+        maskedRounds += plusMask;
+    }
+
+    //TODO here method comments
+    public TVSZ getTVSZ(){ return null;}
+
+    //TODO here method comments
+    public void setTVSZ(){}
+
+    //input: Item removed
+    //method: TODO here
+    //return: void
+    public void removeItem(Item removed){}
 }

141265c5942dbf7a099e6ee2cd1b3753045138da
XanleR <bogardanischool@gmail.com>
2024-03-25 10:46:11 +0100

    Skeleton

---
+++ Beadások/5. Beadás/Skeleton/GameController.java
@@ -0,0 +1,142 @@
+package game;
+
+import game.characters.Student;
+import game.rooms.RegularRoom;
+import game.rooms.Room;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import game.characters.Character;
+
+public class GameController {
+
+
+    //Megadja, hogy hany kor van meg hatra
+    private int remainingRounds;
+
+    //Megadja, hogy hany felhasznalo jatszhat a programmal
+    private int studentCount;
+
+    //Eltarolja a jatekban resztvevo Charactereket
+    private List<Character> listOfCharacters;
+
+    //Eltarolja a jatekban letezo szobakat
+    private List<Room> rooms;
+
+    public GameController(){
+        listOfCharacters = new ArrayList<>();
+        rooms = new ArrayList<>();
+        studentCount = 0;
+    }
+
+    //input: List<Character> characetrs, List<Room> rooms
+    //method: Letrehoz egy jatekot a megadott szobakkal es tanulokkal
+    //return: -
+    public GameController(List<Character> characters, List<Room> rooms){
+        listOfCharacters = characters;
+        this.rooms = rooms;
+        studentCount = listOfCharacters.size();
+    }
+
+    //input: -
+    //method: Kezeli azt az esemenyt, amikor egy Student felveszi a SlideRule targyat
+    //return: void
+    public void slideRulePickedUp(){
+        System.out.println("\t\t\t--> (gameController: GameController).slideRulePickedUp()");
+
+        this.endGame();
+
+        System.out.println("\t\t\t<--");
+    }
+
+    //input: Room slipt
+    //method: A kapott szobat 2 masik szobara osztja
+    //return: void
+    public void slpitRoom(Room split){
+        System.out.println("--> (gameController: GameController).splitRoom(testR1)");
+        Room splittable = new RegularRoom();
+        split.splitTo(splittable);
+
+        Student s1 = new Student();
+        Student s2 = new Student();
+
+        List<Student> studentList = new ArrayList<Student>();
+        studentList.add(s1);
+        studentList.add(s2);
+
+        Scanner scanner = new Scanner(System.in);
+        for(int i=0;i<studentList.size();i++){
+            System.out.println("\t-?- Szeretned, hogy az s" + (i+1) +" hallgato atkeruljon az uj szobaba? (y/n):");
+            String answer = scanner.next();
+
+            if(answer.equals("y")){
+                System.out.println("\t--> (testR2: Room).addCharacter(testS1: Student)");
+                splittable.addCharacter(studentList.get(i));
+                //System.out.println("\t\t<--");
+
+                System.out.println("\t\t--> (testR1: Room).removeCharacter(s"+(i+1)+": Character)");
+                split.removeCharacter(studentList.get(i));
+                System.out.println("\t\t<--");
+
+
+            }
+        }
+
+        System.out.println("<--");
+    }
+
+    //input: Room m1, Room m2
+    //method: A kapott 2 szobabol 1 uj szobat hoz letre
+    //return: void
+    public void mergeRooms(Room m1, Room m2){
+        System.out.println("--> (gameController: GameController).mergeRooms(testR1: Room, testR2: Room)");
+        m1.mergeTo(m2);
+
+        System.out.println("<--");
+    }
+
+    //input: Room newR
+    //method: A kapott Room-ot felveszi a jatek szobai koze
+    //return: void
+    public void addRoom(Room newR){}
+
+    //input: Room remove
+    //method: A kapott szobat kitorli a jatek szobai kozul
+    //return: void
+    public void removeRoom(Room remove){}
+
+    //input: -
+    //method: Elindit egy uj kort
+    //return: void
+    public void newRound(){}
+
+    //input: -
+    //method: Kezeli azt az esemnyt, amikor a jateknak vege van
+    //return: void
+    public void endGame(){
+        System.out.println("\t\t\t\t--> (gameController: GameController).endGame()");
+
+        System.out.println("\t\t\t\t<--");
+    }
+
+    //input: Character removeable
+    //method: A kapott Character-t kitorli a jatekban levo Characterek kozul
+    //return: void
+    public void removeCharacter(Character removeable){
+        System.out.println("\t\t\t--> (gameController: GameController).removeCharacter(removeable: Character)");
+
+        System.out.println("\t\t\t<--");
+    }
+
+    //input: -
+    //method: Elinditja a jatekmenetet
+    //return: void
+    public void play(){}
+
+    //input: -
+    //method: Egy veletlenszeru egesz szamot general az 1-től 6-ig terjedo zart intervallumon.
+    //return: int
+    public int rollDice(){ return 0;}
+}
---
+++ Beadások/5. Beadás/Skeleton/rooms/Room.java
@@ -0,0 +1,176 @@
+package game.rooms;
+
+import game.characters.Character;
+import game.characters.Instructor;
+import game.characters.Student;
+import game.items.Item;
+import game.items.Transistor;
+import game.items.WetRag;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public abstract class Room {
+
+    //A szobaban levo rongyok listaja
+    private List<WetRag> wetRags;
+
+    //A szobaban levo tranzisztor
+    private Transistor transistor;
+
+    //Megadja, hogy maximum hany db karakter lehet a szobaban egyszerre
+    private int maxCharacter;
+
+    //A szobaban levo jatekosok listaja
+    private List<Character> listOfCharacters = new ArrayList<>();
+
+    //input: Character c1
+    //method: Azt az esemenyt kezeli, amikor egy karakter a szobaba lep
+    //return: void
+    public abstract void onEntered(Character c1);
+
+    //input: Character c1
+    //method: Hozzaadja az parameterkent kapott karaktert a szobahoz
+    //return: void
+    public void addCharacter(Character c1) {
+        System.out.println("\t\t--> (testI1: Character).getRoom()");
+        Room from = c1.getRoom();
+        from = new RegularRoom();
+        System.out.println("\t\t<-- from: Room");
+
+        this.isAccessible(from);
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("\t\t-?- At lehet menni az uj szobaba? (y/n): ");
+        String answer2 = scanner2.next();
+
+        if (answer2.equals("y")){
+
+            System.out.println("\t\t--> (testR1: Room).removeCharacter(testI1: Character)");
+            from.removeCharacter(c1);
+            System.out.println("\t\t<--");
+
+            System.out.println("\t\t--> (testR2: Room).onEntered(testI1: Character)");
+            this.onEntered(c1);
+            System.out.println("\t\t<--");
+
+
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.println("\t\t-?- Talalkozott az uj szobaba lepett karakter hallgatoval? (y/n): ");
+            String answer = scanner.next();
+
+            if(answer.equals("y")){
+                Student s1 = new Student();
+                s1.meet(c1);
+            }
+
+            System.out.println("\t\t-?- Talalkozott az uj szobaba lepett karakter tanarral? (y/n): ");
+            answer = scanner.next();
+
+            if(answer.equals("y")){
+                Instructor i1 = new Instructor();
+                i1.meet(c1);
+            }
+        }
+    }
+
+
+    public boolean isAccessible(Room r1) {
+        System.out.println("\t\t--> (TestR2: Room).isAccessible(TestR1: Room)");
+
+        System.out.println("\t\t<-- accessible: boolean");
+        return true;
+    }
+
+    //input: -
+    //method: Visszaadja a szobaban talalhato karaktereket
+    //return: List<Character>
+    public List<Character> getCharacters() {return new ArrayList<>();}
+
+    //input: Character c1
+    //method: Kitorli a parameterkent kapott karaktert a szobabol
+    //return: void
+    public void removeCharacter(Character c1) {
+        System.out.println("\t--> (r1: Room).removeCharacter(testS1: Character)");
+
+        System.out.println("\t<--");
+    }
+
+    //input: Item i1
+    //method: Kitörli a parameterkent kapott Itemet a szobabol
+    //return: void
+    public void removeItem(Item i1) {}
+
+    //input: Item i1
+    //method: Hozzaadja a parameterkent kapott Item-et a szobahoz
+    //return: void
+    public void addItem(Item i1) {}
+
+    //input: List<Item> l1
+    //method: Hozzaadja a szobahoz a parameterkent kapott targyakat
+    //return: void
+    public void addItems(List<Item> l1) {}
+
+    //input: -
+    //method: Visszadja a szobaban talalhato targyakat
+    //return: List<Item>
+    public List<Item> getItems() { return new ArrayList<>();}
+
+    //input: -
+    //method: Visszaadja a szomszedos szobakat
+    //return: List<Item>
+    public List<Room> getNeighbours() {return new ArrayList<>();}
+
+    //input: List<Item> l1
+    //method: A parameterkent kapott lista tagjait hozzaadja a szoba szomszedaihoz
+    //return: void
+    public void addNeighbours(List<Room> l1) {}
+
+    //input: List<Item> l1
+    //method: Kitorli a szoba szomszedai kozul a parameterkent kapottakat
+    //return: void
+    public void removeNeighbours(List<Room> l1) {}
+
+    //input: -
+    //method: Visszaadja a szobaban elhelyezett tranzisztort
+    //return: Transistor
+    public Transistor geTransistor(){
+        System.out.println("\t-->(r1: Room).getTransistor()");
+
+        System.out.println("\t<-- t1: Transistor");
+        return null;
+    }
+
+    //input: Room other
+    //method: Atadja az r2-vel jelolt szobanak az adott szoba targyainak, es a szomszedos szobainak a felet. Igy valosul meg az osztodas
+    //return: void
+    public void splitTo(Room other){
+        System.out.println("\t--> (testR1: Room).splitTo(splittable: Room)");
+        System.out.println("\t<--");
+    }
+
+    //input: Room other
+    //method: Atadja az r2-vel jelolt szobanak az adott szoba osszes szomszedjat, es targyat. Igy valosul meg a szobak osszevonasa
+    //return: void
+    public void mergeTo(Room other){
+        System.out.println("\t--> (testR1: Room).mergeTo(testR2: Room)");
+
+        System.out.println("\t<--");
+    }
+
+    //input: -
+    //method: Atmasolja az r2-vel jelolt szobaba az adott szoba szomszedjait, targyait, es jatekosait
+    //return: void
+    public void copyToRoom(Room r2){}
+
+    //input: Transistor added
+    //method: Hozzaadja a parameterkent kapott tranzisztort a szobahoz
+    //return: void
+    public void addTransistor(Transistor added){}
+
+    //input: WetRag added
+    //method: Hozzaadja a parameterkent kapott rongyot a szobahoz.
+    //return: void
+    public void addWetRag(WetRag added){}
+}
